a06223a5dacf7339758aa5fef35ebba3fdfb9f9e
Leo Reeves <leojreeves@gmail.com>
2016-11-14 14:56:23 +0000

    Added scripts

---
+++ scripts.js
@@ -0,0 +1,34 @@
+// Hide/Show navbar on scroll
+$(window).scroll(function() {
+  var scrollPos = $('body').scrollTop();
+  if (scrollPos > 0) {
+    $(".navbar").css("background", "#344152");
+  } else {
+    $(".navbar").css("background", "Transparent");
+  }
+});
+
+// Fade scroll
+$(window).scroll(function() {
+  $("#main").css("opacity", 1 - $(window).scrollTop() / 500);
+});
+
+// Smooth scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(".btn").mouseup(function(){
+    $(this).blur();
+})
