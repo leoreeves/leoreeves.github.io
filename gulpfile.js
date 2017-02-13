ac68f4526647d8055b3d11233f7a4b7370fd1c06
Leo Reeves <leojreeves@gmail.com>
2017-02-13 20:14:04 +0000

    Applied autoprefixer to all CSS files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
 
 // browserSync
 gulp.task('serve', ['minify-css'], () => {
@@ -16,13 +17,21 @@ gulp.task('serve', ['minify-css'], () => {
   gulp.watch('resources/**/*.js').on('change', browserSync.reload);
 });
 
-// SCSS to CSS
+// SCSS to CSS with autoprefixer
 gulp.task('sass', () => gulp.src('resources/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
     .pipe(gulp.dest('resources/css')));
 
-// Minify CSS
+// Minify CSS with autoprefixer
 gulp.task('minify-css', ['sass'], () => gulp.src(['resources/css/*.css', '!resources/css/*.min.css'])
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(rename({
       suffix: '.min',
