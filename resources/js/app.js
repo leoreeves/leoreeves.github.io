9f8faa63b811b4c6c1d85f54bf2ea35d45e2d32d
Leo Reeves <leojreeves@gmail.com>
2017-02-15 20:21:33 +0000

    Minifed all assets Used Babel on JS to ensure backwards compatability

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,18 @@
+// Defer image load
+function init() {
+  const imgDefer = document.getElementsByTagName('img');
+  for (let i = 0; i < imgDefer.length; i += 1) {
+    if (imgDefer[i].getAttribute('data-src')) {
+      imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
+    }
+  }
+}
+
+window.onload = init;
+
 // Contact form
 const contactform = document.getElementById('contactForm');
-contactform.setAttribute('action', 'https://formspree.io/' + 'leo' + 'j' + 'reeves' + 'portfolio' + '@' + 'gmail' + '.' + 'com');
+contactform.setAttribute('action', 'https://formspree.io/' + 'leo' + 'j' + 'reeves' + '+' + 'portfolio' + '@' + 'gmail' + '.' + 'com');
 
 // Hide/show scroll to top button
 const scrollTopBtn = document.querySelector('.scroll-top');
@@ -11,3 +23,6 @@ document.addEventListener('scroll', () => {
     scrollTopBtn.classList.add('hidden-xs');
   }
 });
+
+// WOW JS Animation
+new WOW().init();
