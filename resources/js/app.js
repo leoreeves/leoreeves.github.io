f1a17f850bd992e5c25b791bded81b06347f8726
Leo Reeves <leojreeves@gmail.com>
2018-03-01 21:59:27 +0000

    [refactor] Remove unused image

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,3 @@
-// Defer image load
-function init() {
-  const imgDefer = document.getElementsByTagName('img');
-  for (let i = 0; i < imgDefer.length; i += 1) {
-    if (imgDefer[i].getAttribute('data-src')) {
-      imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
-    }
-  }
-}
-
-window.onload = init;
-
 // Contact form
 const contactform = document.getElementById('contactForm');
 contactform.setAttribute('action', 'https://formspree.io/' + 'leo' + 'j' + 'reeves' + '+' + 'portfolio' + '@' + 'gmail' + '.' + 'com');
