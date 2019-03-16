c4e7cab110971bcfeeac58a83d8ac836c6fc0f8a
Leo Reeves <leojreeves@gmail.com>
2019-03-16 21:06:18 +0000

    [refactor] Remove unused props

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import styles from './nav.module.scss';
 
-const Nav = (props) => (
+const Nav = () => (
   <nav className={styles.nav}>
     <div className={styles.navContainer}>
       <Link className={styles.navLink} to="/">
