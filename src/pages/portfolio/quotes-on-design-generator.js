2c46536281f83e1069ba60de023fb28089ceac60
Leo Reeves <leojreeves@gmail.com>
2019-03-17 10:59:54 +0000

    [feat] Add quotes on design generator page

---
+++ src/pages/portfolio/quotes-on-design-generator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '../../components/layout/layout';
+
+import Header from '../../components/header/header';
+
+const ContactSuccessPage = () => (
+  <Layout>
+    <Header
+      primaryHeading={'Quotes on Design Generator'}
+      secondaryHeading={'Minimial quote generator powered by Quotes on Design API.'}
+    />
+  </Layout>
+)
+
+export default ContactSuccessPage;
