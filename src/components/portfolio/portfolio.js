6b87d0338155f14d6fad75fa0fa059b8b9a3e270
Leo Reeves <leojreeves@gmail.com>
2019-03-17 10:02:06 +0000

    [feat] Add portfolio items

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,33 +1,66 @@
 import React from 'react';
 
 import styles from './portfolio.module.scss'
+import Image from '../image';
 
-const Portfolio = (props) => (
+const Portfolio = () => (
   <div className={styles.portfolioContainer}>
     <div className={styles.portfolioRow}>
       <div className={styles.portfolioItem}>
-        <div style={{ height: 400, maxWidth: 550, background: 'lightgrey' }}></div>
+        <Image filename="elliott-smith-tribute-page.jpg" />
         <h3>Elliott Smith</h3>
-        <p>Tribute to the musician Elliott Smith, with a biography, discography and photographs of the artist.</p>
+        <p>Tribute to the musician Elliott Smith.</p>
       </div>
       <div className={styles.portfolioItem}>
-        <div style={{ height: 400, maxWidth: 550, background: 'lightgrey' }}></div>
-        <h3>Elliott Smith</h3>
-        <p>Tribute to the musician Elliott Smith, with a biography, discography and photographs of the artist.</p>
+        <Image filename="quotes-on-design-generator.jpg" />
+        <h3>Quotes on Design Generator</h3>
+        <p>
+          Minimial quote generator powered by
+          <span> </span>
+          <a
+            href="https://quotesondesign.com/api-v4-0/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Quotes on Design API
+          </a>
+        .
+        </p>
       </div>
     </div>
     <div className={styles.portfolioRow}>
       <div className={styles.portfolioItem}>
-        <div style={{ height: 400, maxWidth: 550, background: 'lightgrey' }}></div>
-        <h3>Elliott Smith</h3>
-        <p>Tribute to the musician Elliott Smith, with a biography, discography and photographs of the artist.</p>
+        <Image filename="simple-clock.png" />
+        <h3>Simple clock</h3>
+        <p>Simple clock with switchable digital and analogue face.</p>
       </div>
       <div className={styles.portfolioItem}>
-        <div style={{ height: 400, maxWidth: 550, background: 'lightgrey' }}></div>
-        <h3>Elliott Smith</h3>
-        <p>Tribute to the musician Elliott Smith, with a biography, discography and photographs of the artist.</p>
+        <Image filename="uk-vat-calculator.png" />
+        <h3>UK Vat Calculator</h3>
+        <p>Minimal calculator for quickly calculating VAT.</p>
       </div>
-      {/* <Img /> */}
+    </div>
+    <div className={styles.portfolioRow}>
+      <div className={styles.portfolioItem}>
+        <Image filename="hex-colour-generator.jpg" />
+        <h3>Hex Colour Generator</h3>
+        <p>Generate random hex colours.</p>
+      </div>
+      <div className={styles.portfolioItem}>
+        <Image filename="geolocation-weather-app.png" />
+        <h3>Geolocation Weather App</h3>
+        <p>A simple weather app, powered by
+        <span> </span>
+        <a
+            href="https://darksky.net/dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dark Sky API
+        </a>
+        .
+        </p>
+      </div>
     </div>
   </div>
 )
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,12 +4,11 @@ import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
 
 import Header from '../components/header/header';
-import Img from '../components/image';
 import Portfolio from '../components/portfolio/portfolio';
 
 const PortfolioPage = () => (
   <Layout>
-    <SEO title="About" keywords={['Leo Reeves', 'portfolio', 'about']} />
+    <SEO title="Portfolio" keywords={['Leo Reeves', 'portfolio', 'portfolio']} />
     <Header
       primaryHeading={'Portfolio'}
       secondaryHeading={'Featured Work'}
