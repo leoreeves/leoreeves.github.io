d18f73b29bd07cfd23c4fe14cfb89693f51d9d17
Leo Reeves <leojreeves@gmail.com>
2019-10-02 18:10:16 +0100

    [config] Update site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Leo Reeves`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Front-end Developer`,
     author: `@gatsbyjs`,
   },
   plugins: [
