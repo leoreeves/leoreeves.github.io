7f2a4fe5198af694d895ba1511862d6423d8ee19
Leo Reeves <leojreeves@gmail.com>
2020-12-10 23:20:31 +0000

    Updates

---
+++ component---src-pages-contact-success-js-f90817f7a46e34ec4d67.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{Mwea:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a),r=t("Eg6Q"),u=t("8k0H");n.default=function(){return o.a.createElement(u.a,null,o.a.createElement(r.a,{primaryHeading:"Thank you for your message",secondaryHeading:"I will be in touch shortly!",buttonText:"Return Home",buttonRoute:"/"}))}}}]);
+//# sourceMappingURL=component---src-pages-contact-success-js-f90817f7a46e34ec4d67.js.map
