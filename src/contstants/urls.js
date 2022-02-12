4920c892f33912221010f36959f4ea8c3859c4a3
Leo Reeves <leojreeves@gmail.com>
2022-02-12 13:50:39 +0000

    fix: amend link

--- src/contstants/urls.js
+++ src/contstants/urls.js
@@ -1,3 +1,3 @@
 export const CV_URL = 'https://docs.google.com/document/d/1vD0v2erFUkg4HdOV3Tw-Jwlf7QVyec7OBiA4apjtl8M/export?format=pdf'
-export const GITHUB_PROFILE_URL = 'https://github.com/leoreeves/'
+export const GITHUB_URL = 'https://github.com/leoreeves/'
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/leoreeves/'
